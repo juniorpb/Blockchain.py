1ebce05f3162b9c15f8cf4f86a58c4b7873eb2c0
Junnnior <juniorfoxld@hotmail.com>
2019-07-17 22:59:32 -0300

    Implementando dificuldades no blockchain

--- Block.py
+++ Block.py
@@ -1,56 +1,82 @@
 import hashlib
 import time
+import binascii
 
 
 class Block:
-    def __init__(self, index, previousHash, timestamp, data, hash):
+    def __init__(self, index, previousHash, timestamp, data, hash, difficulty, nonce):
         self.index = index
         self.previousHash = previousHash
         self.timestamp = timestamp
         self.data = data
         self.hash = hash
+        self.difficulty = difficulty
+        self.nonce = nonce
 
 class Blockchain:
     
-    # adiciona bloco á Blockchain
+    # add block in Blockchain
     def __init__(self, genesisBlock):
         self.__chain = []
         
-        # adiciona primeiro block 
+        # add first block 
         self.__chain.append(genesisBlock)
 
-    # pega ultimo block na Blockchain
+    # get last block in Blockchain
     def getLatesBlock(self):
-        return self.__chain(-1)
+        return self.__chain[-1] # -1 return last element of list
 
-    # gera novo block
-    def generateNextBlock():
-        previousBlock       = getLatesBlock()
+    # generetes new block
+    def generateNextBlock(self, data):
+        previousBlock       = self.getLatesBlock()
         nextIndex           = previousBlock.index + 1
         nextTimesTamp       = int(round(time.time() * 1000))
         nextPreviousHash    = previousBlock.hash
         newBlock = Block(nextIndex, nextPreviousHash, nextTimesTamp, data, calculateHash(nextIndex, nextPreviousHash, nextTimesTamp, data))
 
-        if validatingBlock(newBlock) == True:
+        if self.validatingBlock(newBlock) == True:
             self.__chain.append(newBlock)
     
-    # validação do novo block
+    # validation new block
     def validatingBlock(self, newBlock):
         previousBlock = self.getLatesBlock()
 
-        # valida index do novo bloco com o anterior
+        # validation the index of new block with previous 
         if previousBlock.index + 1 != newBlock.index:
             return False
         
-        # valida igualdade do hash anterior
-        elif self.previousBlock.hash != newBlock.previousHash:
+        # equality validation of previous hash
+        elif previousBlock.hash != newBlock.previousHash:
             return False
 
         return True
 
-def calculateHash(index, previousHash, timestamp, data):
-    return hashlib.sha256(str(index) + previousHash + str(timestamp) + data).hexdigest()
+    # the difficulty level is based on the quantity 
+    # of digits 0 (zeros) at the start of the hash on binary basis.
+    def hashMatchesDefficulty(self, hash, difficulty):
+
+        # translate hash of binary
+        hashBinary = binascii.unhexlify(hash)
+
+        # create level of difficulty
+        requiredPrefix = '0' * int(difficulty)
+
+        return hashBinary.startswith(requiredPrefix)
+
+    def findBlock(self, index, previousHash, timestamp, data, difficulty):
+        nonce = 0
+        while True:
+            hash = self.calculateHash(index, previousHash, timestamp, data, difficulty, nonce)
+            if self.hashMatchesDefficulty(hash, difficulty):
+                block = Block(index, previousHash, timestamp, data, difficulty, nonce)
+                return block
+            
+            nonce += 1
+         
+
+def calculateHash(index, previousHash, timestamp, data, difficulty, nonce):
+    return hashlib.sha256((str(index) + previousHash + str(timestamp) + data + str(difficulty) + str(nonce)).encode('utf-8')).hexdigest()
 
 
 timesTampVar = int(round(time.time() * 1000))
-genesisBlock = Block(0, "", timesTampVar, "Genesis Block", calculateHash(, "", timesTampVar, "Genesis Block"))
+genesisBlock = Block(0, "", timesTampVar, "Genesis Block", calculateHash(0, "", timesTampVar, "Genesis Block"))
